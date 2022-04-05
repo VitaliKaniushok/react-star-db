e8d1f81c335257770b3d5235bfbcb4a29b0c83e9
VK_MSM <vitali.kaniushok@brightforce.pl>
2022-04-06 00:55:30 +0200

    Updates

---
+++ precache-manifest.7dd0a08cad3d4b7ecc74d406062ae135.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbe1e563efd99e968e958c739d063ed",
+    "url": "/react-star-db/index.html"
+  },
+  {
+    "revision": "8ba5a98f0870e905db31",
+    "url": "/react-star-db/static/css/main.4c739866.chunk.css"
+  },
+  {
+    "revision": "38982c68e983f8079e87",
+    "url": "/react-star-db/static/js/2.becc1418.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/react-star-db/static/js/2.becc1418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ba5a98f0870e905db31",
+    "url": "/react-star-db/static/js/main.f6ce4f52.chunk.js"
+  },
+  {
+    "revision": "07ab8cbadeaf3f041caf",
+    "url": "/react-star-db/static/js/runtime-main.1dd8fe17.js"
+  }
+]);
